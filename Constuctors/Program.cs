85defa6e56a95f2f84d3a0617ee2d292c2c02532
Рената Цікалова <ts.renata98@gmail.com>
2022-01-20 18:42:42 +0200

    Add project files.

---
+++ Constuctors/Program.cs
@@ -0,0 +1,20 @@
+﻿using ListsLibrary;
+using System;
+
+namespace Constuctors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList<int> array = new ArrayList<int>(5);
+
+            Console.WriteLine(array.IndexByItem(10));
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+        }
+    }
+}
